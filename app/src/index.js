2d3fc0fd6781a36dc50b3003d2f2973f775d3e10
sneh-todarmal <sneh-todarmal@ecosmob.com>
2026-04-03 16:56:52 +0530

    dev made changes

--- app/src/index.js
+++ app/src/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.json({
     status: 'ok',
-    message: 'Jenkins pipeline app is running',
+    message: 'Devops pipeline app is running',
     environment: process.env.NODE_ENV || 'development'
   });
 });
